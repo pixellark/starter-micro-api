0442487cd3759c793623872bcb983b192dd97a6f
pixellark <136720900+pixellark@users.noreply.github.com>
2023-12-05 21:08:37 +0530

    fgh

--- index.js
+++ index.js
@@ -237,5 +237,5 @@ function get_access_token() {
 }
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
+    console.log(`Server listening at https://silly-puce-dirndl.cyclic.app`)
 })
